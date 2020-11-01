dca6d129ce16826403a7521ae575ec725ce71fdd
yufiy <yuliya@yandex.ru>
2020-11-01 19:32:24 +0300

    Send repository

---
+++ src/test/java/steps_definitions/GoogleRunnerTest.java
@@ -0,0 +1,21 @@
+package steps_definitions;
+
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        monochrome = true,
+        features = "src/test/resources/search.feature",
+        snippets = SnippetType.CAMELCASE,
+        glue = {"java.steps_definitions"}
+
+)
+public class GoogleRunnerTest {
+}
